19c36cb9e2704d06081011fea3d8be0b0bb2421b
Raghav-Gupta22 <raghavgupta216@gmail.com>
2021-05-10 22:44:03 +0530

    Added a method that will display a toast after successful completion of audio playing.

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
@@ -31,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "I'm Done!", Toast.LENGTH_SHORT).show();
             }
         });
 
